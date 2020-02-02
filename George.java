a78660af5b9b49fe215b59a9f36b7e21c6132b92
paperclipstudio <paperclipstudio@gmail.com>
2020-02-02 14:57:32 +0000

    TidyUp

--- George.java
+++ George.java
@@ -9,33 +9,31 @@ class George{
 
   public static void main(String[] args) {
 
-    // Parsing File
+    //// Parsing File
     ArrayList<Ride> rides = new ArrayList<Ride>();
     try {
       rides = Parse.parseRidesFromFile("a_example.in");
     } catch (Exception e) {
       System.out.println("File not found");
     }
-
-
     Routes currentRoute = new Routes();
-    // Packing Routes
-    for (int i=0; i<rides.size();i++) {
-      System.out.println(rides.get(0));
-      currentRoute.addRide(rides.get(i));
-      System.out.printf("Points: %3d, Distance: %3d\n",
-        rides.get(i).getPoints(0),
-        rides.get(i).getDistance()
-        );
-    }
 
+
+    //// Packing Routes
+
+    // Put each Ride into Unique Routes
     Routes[] solution = new Routes[2];
-
     solution[0] = currentRoute;
     solution[1] = new Routes();
-    // Output Parsing
 
-    System.out.print(Parse.parseRoutesToString(currentRoute));
+    // Join route together
+
+    // loop untill X number remains
+    for (int i=0; i<rides.size();i++) {
+      currentRoute.addRide(rides.get(i));
+    }
+
+    //// Output Parsing
     try{
       Parse.parseRoutesToFile(solution, "GeorgeOut.out");
     } catch(IOException e) {
